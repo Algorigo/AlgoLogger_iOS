09092242797f5e2cd999b5563382cdb10ea72d93
Rouddy <rouddy@naver.com>
2024-03-07 10:53:08 +0900

    log file의 확장자가 .log로 고정이었던 버그 수정

--- AlgoLogger/Sources/AlgoLogger/Destination/RotatingFileDestination.swift
+++ AlgoLogger/Sources/AlgoLogger/Destination/RotatingFileDestination.swift
@@ -35,16 +35,30 @@ public class RotatingFileDestination: AutoRotatingFileDestination {
     }
     
     public struct LogFile {
-        let base: String
+        let basePrefix: String
+        let baseExtention: String
         public let rotatedDate: Date
         public let postfix: String
         
         internal var path: String {
-            "\(base)\(RotatingFileDestination.formatter.string(from: rotatedDate))\(postfix).log"
+            "\(basePrefix)\(RotatingFileDestination.formatter.string(from: rotatedDate))\(postfix)\(baseExtention)"
+        }
+        
+        private init(basePrefix: String, baseExtention: String, rotatedDate: Date, postfix: String) {
+            self.basePrefix = basePrefix
+            self.baseExtention = baseExtention
+            self.rotatedDate = rotatedDate
+            self.postfix = postfix
         }
         
         init(base: String, rotatedDate: Date = Date(), postfix: String = "") {
-            self.base = base
+            if let lastDot = base.lastIndex(of: ".") {
+                self.basePrefix = String(base[..<lastDot])
+                self.baseExtention = String(base[lastDot...])
+            } else {
+                self.basePrefix = base
+                self.baseExtention = ""
+            }
             self.rotatedDate = rotatedDate
             self.postfix = postfix
         }
@@ -53,7 +67,7 @@ public class RotatingFileDestination: AutoRotatingFileDestination {
             let rotatedDateString: String
             if #available(iOS 16.0, *) {
                 let fullPath = url.path(percentEncoded: true)
-                guard let regex = try? Regex("^(.*)([0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2})(.*).log$") else {
+                guard let regex = try? Regex("^(.*)([0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2})(.*)$") else {
                     return nil
                 }
                 guard let match = try? regex.firstMatch(in: fullPath) else {
@@ -68,12 +82,19 @@ public class RotatingFileDestination: AutoRotatingFileDestination {
                 guard let match3 = match[3].value as? Substring else {
                     return nil
                 }
-                self.base = String(describing: match1)
-                rotatedDateString = String(describing: match2)
-                self.postfix = String(describing: match3)
+                self.basePrefix = String(match1)
+                rotatedDateString = String(match2)
+                let postfixAndExtention = String(match3)
+                if let lastDot = postfixAndExtention.lastIndex(of: ".") {
+                    self.postfix = String(postfixAndExtention[..<lastDot])
+                    self.baseExtention = String(postfixAndExtention[lastDot...])
+                } else {
+                    self.postfix = postfixAndExtention
+                    self.baseExtention = ""
+                }
             } else {
                 let fullPath = url.path
-                guard let regex = try? NSRegularExpression(pattern: "^(.*)([0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2})(.*).log$", options: []) else {
+                guard let regex = try? NSRegularExpression(pattern: "^(.*)([0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2})(.*)$", options: []) else {
                     return nil
                 }
                 guard let match = regex.firstMatch(in: fullPath, options: [], range: NSRange(location: 0, length: fullPath.count)) else {
@@ -88,9 +109,16 @@ public class RotatingFileDestination: AutoRotatingFileDestination {
                 guard let range3 = Range(match.range(at: 3), in: fullPath) else {
                     return nil
                 }
-                self.base = String(fullPath[range1])
+                self.basePrefix = String(fullPath[range1])
                 rotatedDateString = String(fullPath[range2])
-                self.postfix = String(fullPath[range3])
+                let postfixAndExtention = String(fullPath[range3])
+                if let lastDot = postfixAndExtention.lastIndex(of: ".") {
+                    self.postfix = String(postfixAndExtention[..<lastDot])
+                    self.baseExtention = String(postfixAndExtention[lastDot...])
+                } else {
+                    self.postfix = postfixAndExtention
+                    self.baseExtention = ""
+                }
             }
             guard let rotatedDate = RotatingFileDestination.formatter.date(from: rotatedDateString) else {
                 return nil
@@ -99,7 +127,7 @@ public class RotatingFileDestination: AutoRotatingFileDestination {
         }
 
         func withPostfix(postfix: String) -> LogFile {
-            return LogFile(base: base, rotatedDate: rotatedDate, postfix: postfix)
+            return LogFile(basePrefix: basePrefix, baseExtention: baseExtention, rotatedDate: rotatedDate, postfix: postfix)
         }
     }
     
